59281b71cc31cc335f02162c488f0cff796555cf
Sergey Baranov <workinggrid@gmail.com>
2025-02-25 22:57:42 +0300

    added logic

--- Day7 - Advanced Music Player/js/musicList.js
+++ Day7 - Advanced Music Player/js/musicList.js
@@ -4,25 +4,25 @@ export let allMusic = [
   {
     songTitle: "ブルーバード", // song title
     songAuthor: "いきものがかり", //artist name
-    comerImg: "cover-1", //cover name
+    coverImg: "cover-1", //cover name
     src: "blueBird" //song file name
   },
   {
     songTitle: "Morning", // song title
     songAuthor: "Toshiro Masuda", //artist name
-    comerImg: "cover-1", //cover name
+    coverImg: "cover-1", //cover name
     src: "morning" //song file name
   },
   {
     songTitle: "Ванюша (ОСТ Последний богатырь)", // song title
     songAuthor: "Моя Мишель", //artist name
-    comerImg: "cover-2", //cover name
+    coverImg: "cover-2", //cover name
     src: "lastBogatir" //song file name
   },
   {
     songTitle: "Hana no Youni (Huanabi)", // song title
     songAuthor: "Matsu Takako", //artist name
-    comerImg: "cover-3", //cover name
+    coverImg: "cover-3", //cover name
     src: "HanaNoYouni" //song file name
   }
 ];
--- Day7 - Advanced Music Player/js/player.js
+++ Day7 - Advanced Music Player/js/player.js
@@ -1,15 +1,15 @@
 // import { allMusic } from "./musicList";
 
 const playerWrapper = document.querySelector(".playerWrapper"); // создаем переменную для всего плеера
-const coverImg = document.querySelector("coverImg"); // создаем переменную для работы с обложкой
-const songTitle = document.querySelector(".songTitle"); //переменная для работы с наименованием песни 
-const songAuthor = document.querySelector(".songAuthor"); //переменная для работы с исполнителем песни
-const playPauseBtn = document.querySelector(".playSong"); // переменная для работы со стартом и паузой 
-const prevBtn = document.querySelector(".previousSong"); //переменная для предыдущей песни
-const nextBtn = document.querySelector(".nextSong"); //переменная для следующей песни
+const coverImg = playerWrapper.querySelector("coverImg"); // создаем переменную для работы с обложкой
+const songTitle = playerWrapper.querySelector(".songTitle"); //переменная для работы с наименованием песни 
+const songAuthor = playerWrapper.querySelector(".songAuthor"); //переменная для работы с исполнителем песни
+const playPauseBtn = playerWrapper.querySelector(".playSong"); // переменная для работы со стартом и паузой 
+const prevBtn = playerWrapper.querySelector("#previousSong"); //переменная для предыдущей песни
+const nextBtn = playerWrapper.querySelector("#nextSong"); //переменная для следующей песни
 const mainAudio = document.querySelector("#mainAudio"); //переменная для контента
-const progressBarArea = document.querySelector(".progressBarArea"); //переменная для области прокрутки аудиоконтента
-const progressBar = document.querySelector(".progressBar");//переменная для прогрессбара
+const progressBarArea = playerWrapper.querySelector(".progressBarArea"); //переменная для области прокрутки аудиоконтента
+const progressBar = progressBarArea.querySelector(".progressBar");//переменная для прогрессбара
 
 let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
 isMusicPaused = true;
@@ -58,3 +58,26 @@ playPauseBtn.addEventListener("click", () => {
   isMusicPlay ? pauseMusic() : playMusic();
 }
  )
+
+prevBtn.addEventListener("click", () => {
+  prevSong();
+})
+
+nextBtn.addEventListener("click", () => {
+  nextSong();
+})
+
+mainAudio.addEventListener("timeupdate", (e) => {
+  const currentTime = e.target.currentTime;
+  const duration = e.target.duration;
+  let progressWidth = (currentTime/duration)*100;
+  progressBar.computedStyleMap.width = `${progressWidth}%`;
+
+  let musicCurrentTime = playerWrapper.querySelector(".currentTime"); // Assuming formatTime is a function to format the time
+  let musicDuration = playerWrapper.querySelector(".maxDuration");
+
+  // Update the UI with the current time and duration
+  currentTimeElement.innerText = musicCurrentTime;
+  durationElement.innerText = musicDuration;
+
+})
