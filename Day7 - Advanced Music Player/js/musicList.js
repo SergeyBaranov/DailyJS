7c2f2a96b299d924feffb691f79b82a052bde219
Sergey Baranov <workinggrid@gmail.com>
2025-03-02 19:40:26 +0300

    start/stop/prev/next are working, but have an issue with all songs

--- Day7 - Advanced Music Player/js/musicList.js
+++ Day7 - Advanced Music Player/js/musicList.js
@@ -1,28 +1,27 @@
 //создаем массив песен
-
-export let allMusic = [
+let allMusic = [
   {
     songTitle: "ブルーバード", // song title
     songAuthor: "いきものがかり", //artist name
-    img: "cover-1", //cover name
+    mainCover: "cover-1", //cover name
     src: "blueBird" //song file name
   },
   {
     songTitle: "Morning", // song title
     songAuthor: "Toshiro Masuda", //artist name
-    img: "cover-1", //cover name
+    mainCover: "cover-1", //cover name
     src: "morning" //song file name
   },
   {
     songTitle: "Ванюша (ОСТ Последний богатырь)", // song title
     songAuthor: "Моя Мишель", //artist name
-    img: "cover-2", //cover name
+    mainCover: "cover-2", //cover name
     src: "lastBogatir" //song file name
   },
   {
     songTitle: "Hana no Youni (Huanabi)", // song title
     songAuthor: "Matsu Takako", //artist name
-    img: "cover-3", //cover name
+    mainCover: "cover-3", //cover name
     src: "HanaNoYouni" //song file name
   }
 ];
--- Day7 - Advanced Music Player/js/player.js
+++ Day7 - Advanced Music Player/js/player.js
@@ -1,4 +1,4 @@
-// import { allMusic } from "./musicList";
+// Load Music
 
 const playerWrapper = document.querySelector(".playerWrapper"); // создаем переменную для всего плеера
 const coverImg = playerWrapper.querySelector(".mainCover"); // создаем переменную для работы с обложкой
@@ -14,54 +14,37 @@ const progressBar = progressBarArea.querySelector(".progressBar");//переме
 let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
 isMusicPaused = true;
 
-window.addEventListener("load" , () => {
-  loadMusic(musicIndex)
-});
+window.addEventListener("load", () => { //когда окно загружено  
+  loadMusic(musicIndex); //загружаем песню
+})
 
-function loadMusic(indexNumber){
-  songTitle.innerText = allMusic[indexNumber - 1].songTitle;
-  songAuthor.innerText = allMusic[indexNumber - 1].songAuthor;
-  coverImg.src = `img/albumCovers/${allMusic[indexNumber - 1].src}.jpg`
-  mainAudio.src = `media/${allMusic[indexNumber - 1].src}.mp3`
+function loadMusic(indexNumber){ //функция для загрузки песни
+  songTitle.innerText = allMusic[indexNumber - 1].songTitle; //выводим наименование песни
+  songAuthor.innerText = allMusic[indexNumber - 1].songAuthor; //выводим исполнителя песни
+  coverImg.src = `img/albumCovers/${allMusic[indexNumber - 1].mainCover}.jpg`; //выводим обложку песни
+  mainAudio.src = `media/${allMusic[indexNumber - 1].src}.mp3` //выводим песню
 }
 
-
-function playMusic() { // создаем функцию playPause
-  if(playPauseBtn.classList.contains('playSong')) { 
-    coverImg.classList.add('rotate');
-    playPauseBtn.classList.remove('playSong');
-    playPauseBtn.classList.add('pauseSong');   
-    playPauseBtn.innerHTML = '<img src="img/icons/pauseIcon.svg" alt="play song">';
-    mainAudio.play(); // запускаем аудио
-  } else { // иначе
-    playPauseBtn.classList.remove('pauseSong'); 
-    coverImg.classList.remove('rotate');
-    playPauseBtn.classList.add('playSong');
-    playPauseBtn.innerHTML = '<img src="img/icons/playIcon.svg" alt="play song">';
-    mainAudio.pause(); // останавливаем аудио
-  }
+function playMusic() {
+  coverImg.classList.add('rotate');  
+  playPauseBtn.classList.add("pauseSong");
+  playPauseBtn.innerHTML = '<img src="img/icons/pauseIcon.svg" alt="play song">';
+  mainAudio.play();
 }
 
-// function playMusic() {
-//   coverImg.classList.add('rotate');  
-//   playPauseBtn.classList.add("pauseSong");
-//   playPauseBtn.innerHTML = '<img src="img/icons/pauseIcon.svg" alt="play song">';
-//   mainAudio.play();
-// }
+function pauseMusic() {
+  playPauseBtn.classList.add("playSong");
+  playPauseBtn.classList.remove('pauseSong');
+  coverImg.classList.remove('rotate');
+  playPauseBtn.innerHTML = '<img src="img/icons/playIcon.svg" alt="pause song">';
+  mainAudio.pause();
+}
 
-// function pauseMusic() {
-//   playPauseBtn.classList.add("playSong");
-//   playPauseBtn.classList.remove('pauseSong');
-//   coverImg.classList.remove('rotate');
-//   playPauseBtn.innerHTML = '<img src="img/icons/playIcon.svg" alt="pause song">';
-//   mainAudio.pause();
-// }
-
-function prevSong() {
-  musicIndex--;
-  musicIndex = musicIndex < 1 ? allMusic.length : musicIndex;
-  loadMusic(musicIndex);
-  playMusic();
+function prevSong() { //функция для предыдущей песни  
+  musicIndex--; //уменьшаем индекс на 1
+  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex; //если индекс меньше 1, то индекс равен длине массива
+  loadMusic(musicIndex); //загружаем песню
+  playMusic(); //запускаем песню
 }
 
 function nextSong() {
@@ -77,30 +60,30 @@ playPauseBtn.addEventListener("click", () => {
 }
  )
 
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener("click", () => { //когда нажимаем на кнопку предыдущей песни
   prevSong();
 })
 
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener("click", () => { //когда нажимаем на кнопку следующей песни
   nextSong();
 })
 
-mainAudio.addEventListener("timeupdate", (e) => {
-  const currentTime = e.target.currentTime;
-  const duration = e.target.duration;
-  let progressWidth = (currentTime/duration)*100;
-  progressBar.computedStyleMap.width = `${progressWidth}%`;
+// mainAudio.addEventListener("timeupdate", (e) => { //когда аудио обновляется
+//   const currentTime = e.target.currentTime; // текущее время
+//   const duration = e.target.duration; //длительность песни
+//   let progressWidth = (currentTime/duration)*100; //ширина прогрессбара
+//   progressBar.computedStyleMap.width = `${progressWidth}%`; //присваиваем ширину прогрессбару
 
-  let musicCurrentTime = playerWrapper.querySelector(".currentTime"); // Assuming formatTime is a function to format the time
-  let musicDuration = playerWrapper.querySelector(".maxDuration");
+//   let musicCurrentTime = playerWrapper.querySelector(".currentTime"); //переменная для текущего времени
+//   let musicDuration = playerWrapper.querySelector(".maxDuration"); //переменная для длительности песни
 
-  mainAudio.addEventListener("loadedData", () => {
-    let mainAudioDUration = mainAudio.duration;
-    let totalMin = Math.floor(mainAudioDUration / 60);
-    let totalSec = Math.floor(mainAudioDUration % 60);
-    if(totalSec < 10) {
-      totalSec = `0${totalSec}`;
-    }
-    musicDuration.innerText = `${totalMin}:${totalSec}`;
-  })
-})
+//   mainAudio.addEventListener("loadedData", () => { //когда аудио загружено
+//     let mainAudioDUration = mainAudio.duration; //длительность песни
+//     let totalMin = Math.floor(mainAudioDUration / 60); //минуты
+//     let totalSec = Math.floor(mainAudioDUration % 60);//  секунды
+//     if(totalSec < 10) { //если секунды меньше 10
+//       totalSec = `0${totalSec}`; // добавляем 0
+//     }
+//     musicDuration.innerText = `${totalMin}:${totalSec}`; // выводим длительность песни
+//   })
+// })
